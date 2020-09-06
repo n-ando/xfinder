b4b79b12ee159406b70e6ba48c36bc847ee908a7
n-ando <Noriaki.Ando@gmail.com>
2020-09-06 18:27:15 +0900

    Modification for EXE on windows (stdin/out/err issue). Cleanup Makefile/build.cmd. Executable added.

--- build.cmd
+++ build.cmd
@@ -1,12 +1,30 @@
-rd /Q /S dist
-rd /Q /S build
+REM ============================================================
+REM Building xfinder.exe
+REM
+REM @file build.cmd
+REM @brief xfinder executable build batchfile
+REM @author Noriaki Ando <Noriaki.Ando@gmail.com>
+REM @copyright Copyright (C) 2020 Noriaki Ando, All right reserved.
+REM
+REM This batch file generate xfinder.py's executable xfinder.exe
+REM How to use:
+REM build.cmd       .... build xfiner.exe (exe will be in the "dist")
+REM build.cmd       .... clean-up generated files
+REM ============================================================
 
-mkdir dist
+REM Always clean generated files
+del /S /Q __pycache__
+rd /S /Q __pycache__
+del /S /Q build
+rd /S /Q build
+del /S /Q dist
+rd /S /Q dist
+del /S /Q xfinder.spec
 
-copy icons\raspi.ico dist
-C:\python27\python.exe setup.py py2exe
-"C:\Program Files (x86)\NSIS\makensis.exe" setup.nsi
+REM build target goes to END
+if x%1 == xclean goto END
 
-copy icons\raspi.ico dist
-C:\python27\python.exe setup.py py2exe
-"C:\Program Files (x86)\NSIS\makensis.exe" setup.nsi
+REM Building exe
+pyinstaller xfinder.py --onefile --noconsole --icon=".\\icons\\raspi.ico" --add-data=".\\icons\\raspi.ico;.\\icons"
+
+:END
